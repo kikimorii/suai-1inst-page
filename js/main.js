86b9b6f5817660d9b24d326e95ae17b780e28c8a
Oleg Fateev <fatev010405@gmail.com>
2023-12-04 04:32:47 +0300

    add menu-burger on mobile ver.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 let menuBtn = document.querySelector('.menu_btn');
+let Bur = document.querySelector('.header_menu-burger');
+let Body = document.querySelector('body');
 
 menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
-	menu.classList.toggle('active');
+	Bur.classList.toggle('active');
+	Body.classList.toggle('lock');
 })
